da9e7656b8c294d92953567f62d94e3c955e25f5
Russ Taylor <russ@reliasolve.com>
2021-06-10 12:23:27 -0400

    Checks for errors in mp_geo and reports them as failures.  Does rounding on results to get 2 digits from SuiteName output

--- compare.sh
+++ compare.sh
@@ -51,7 +51,10 @@ for f in $files; do
   # Note that there are absolute-percentile-RNAsuiteness and other prefixed versions
   # that we want to ignore.
   line=`gunzip < $fval | grep ' RNAsuiteness'`
-  if [ -z "$line" ] ; then continue ; fi
+  if [ -z "$line" ] ; then
+    if [ -n "$VERBOSE" ] ; then echo "No RNAsuiteness in $fval (skipping)" ; fi
+    continue
+  fi
   val=`echo "$line" | sed -r 's/[ [:alnum:]]+=/\n&/g' | awk -F= '$1==" RNAsuiteness"{print $2}'`
   # Sometimes we get a second word "bonds_" after the quoted number, so we strip it.
   val=`echo $val | awk '{print $1;}'`
@@ -69,23 +72,40 @@ for f in $files; do
   # Get the full mmCIF file name
   d2=`echo $f | cut -d/ -f 2`
   d3=`echo $f | cut -d/ -f 3`
-  cval=mmCIF/$d2/$d3.cif.gz
-  if [ -n "$VERBOSE" ] ; then echo "Comparing $cval" ; fi
+  cname=mmCIF/$d2/$d3.cif.gz
+  if [ -n "$VERBOSE" ] ; then echo "Comparing $cname" ; fi
 
   # Decompress the file after making sure the file exists.
-  if [ ! -f $cval ] ; then continue ; fi
+  if [ ! -f $cname ] ; then continue ; fi
   tfile="./tmp.cif"
-  gunzip < $cval > $tfile
+  gunzip < $cname > $tfile
 
-  # Run to get the output of the report
-  suite=`mmtbx.mp_geo rna_backbone=True $tfile | phenix.suitename -report -oneline -pointIDfields 7 -altIDfield 6`
+  # Run to get the output of the report.
+  # If either program returns failure, report the failure.
+  t2file="./tmp2.out"
+  mmtbx.mp_geo rna_backbone=True $tfile > $t2file
+  if [ $? -ne 0 ] ; then
+    echo "Error running mp_geo for $cname"
+    let "failed++"
+    continue
+  fi
+  suite=`phenix.suitename -report -oneline -pointIDfields 7 -altIDfield 6 < $t2file`
 
   # Parse to pull out average suiteness== 0.694 (for one particular file) and then round.
   sval=`echo "$suite" | awk -F"average suiteness==" '{print $2}'`
   # Remove blank lines.
   # Pull only the first report, which will be the total; and only its first word
   sval=`echo "$sval" | grep -v -e '^$' | head -1 | awk '{print $1}'`
-  echo "val = $val, sval = $sval"
+  # Use Banker's rounding to find the nearest 2-digit number (rounds 0.5 to even).
+  sval=`printf "%.2f" $sval`
+
+  # Report failure on this file if it happens.
+  if [ `echo $sval | wc -w` -ne 1 ] ; then
+    echo "Error computing suiteness for $cname"
+    let "failed++"
+    continue
+  fi
+  echo "$f val = $val, sval = $sval"
 
 done
 
